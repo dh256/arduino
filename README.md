75bce31dd388249ea4c4d8b0ec21bbdd8cf77b60
dh256 <david.hanley12@btinternet.com>
2017-05-22 21:18:35 +0100

    Modified traffic_lights to have different sequence depending on country; Tidied up README.MD

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # Arduino
 Arduino sketch files and associated circuit diagrams
 
-##blinking_led_light
+## blinking_led_light
   Make a LED light blink every x miliseconds
-  Circuit:
-    ![LED Light Circuit](circuits/SingleLED.jpg)
   
-##traffic_light
-  A traffic light sequence
-  Circuit:
+  Circuit Picture:
+    ![LED Light Circuit](circuits/SingleLED.jpg)
+
+  Notes:
+  - Resistor: 220 Ohms
+  
+## traffic_light
+  A traffic light sequence. User can enter "U" for UK and "O" for other country.
+  
+  Circuit Picture:
     ![Traffic Lights Circuit](circuits/TrafficLights.jpg)
   
-  
+  Notes:
+  - Each LED should be protected with an appropriate resistor (not shown here for simplicity)
--- traffic_lights/traffic_lights.ino
+++ traffic_lights/traffic_lights.ino
@@ -1,5 +1,9 @@
 // Simple traffic lights sequence
 // David Hanley, May 2017
+//
+// User can select U (UK) or O (Other) to show country specific sequence
+// Use SerialMonitor to send country to Arduino
+//
 
 int red_light_pin = 7;           // pin to use
 int amber_light_pin = 8;
@@ -10,23 +14,41 @@ int hold_red_amber = 1000;
 int hold_green = 4000;
 int hold_amber = 1000;
 
+char country = 'O';
+
 void setup() {
   // put your setup code here, to run once:
+
+  // set up pins for outpyt
   pinMode(red_light_pin, OUTPUT);
   pinMode(green_light_pin, OUTPUT);
   pinMode(amber_light_pin, OUTPUT);
+
+  // open serial port
+  Serial.begin(9600);
+  Serial.println("Enter country 'U' for UK; 'O' for OTHER: ");
+  while (Serial.available() == 0) {}    // wait until a character is sent
+  country = Serial.read();
+  Serial.println("Starting sequence");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("STOP");
   digitalWrite(red_light_pin, HIGH);
   delay(hold_red);
-  digitalWrite(amber_light_pin, HIGH);
-  delay(hold_red_amber);
+  if (country == 'U') {         
+    // UK uses red-amber - get ready to go
+    Serial.println("GET READY TO GO");
+    digitalWrite(amber_light_pin, HIGH);
+    delay(hold_red_amber);
+    digitalWrite(amber_light_pin, LOW);
+  }
   digitalWrite(red_light_pin, LOW);
-  digitalWrite(amber_light_pin, LOW);
+  Serial.println("GO");
   digitalWrite(green_light_pin, HIGH);
   delay(hold_green);
+  Serial.println("GET READY TO STOP");
   digitalWrite(amber_light_pin, HIGH);
   digitalWrite(green_light_pin, LOW);
   delay(hold_amber);
